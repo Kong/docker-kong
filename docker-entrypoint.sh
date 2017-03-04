8c38f640c26ec5c628da39d73ddb10edef7cc04e
CK Dev <c-knowles@users.noreply.github.com>
2017-03-04 13:06:06 +0800

    Ensure Kong logs reach docker logging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -e
 
+# Make a pipe for the logs so we can ensure Kong logs get directed to docker logging
+# see https://github.com/docker/docker/issues/6880
+# also, https://github.com/docker/docker/issues/31106, https://github.com/docker/docker/issues/31243
+# https://github.com/docker/docker/pull/16468, https://github.com/behance/docker-nginx/pull/51
+rm -f /tmp/logpipe
+mkfifo -m 666 /tmp/logpipe
+# This child process will still receive signals as per https://github.com/Yelp/dumb-init#session-behavior
+cat <> /tmp/logpipe 1>&2 &
+
 # Disabling nginx daemon mode
 export KONG_NGINX_DAEMON="off"
 
