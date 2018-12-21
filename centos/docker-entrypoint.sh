67a8f7aee64cc371bf0e6dedb9d70cd1f731d8de
Thibault Charbonnier <thibaultcha@me.com>
2018-12-21 23:04:32 +0000

    feat(entrypoint) auto-setcap if transparent
    * auto-detect 'transparent' suffix in listeners to trigger setcap
    * ability to force setcap with new `SET_CAP_NET_RAW` env variable
    * run master and worker processes as kong with su-exec
        * requires a workaround for /dev/stdout and /dev/stderr output
    
    Unfortunately, `kong prepare` still has to be executed as root (as it is
    the case prior to this patch), but trying to execute it as the kong user
    doesn't seem to work. Will revisit later.
    
    Co-authored-by: Colin Hutchinson <chutchic@gmail.com>

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -3,21 +3,32 @@ set -e
 
 export KONG_NGINX_DAEMON=off
 
+has_transparent() {
+  echo "$1" | grep -E "[^\s,]+\s+transparent\b" >/dev/null
+}
+
 if [[ "$1" == "kong" ]]; then
   PREFIX=${KONG_PREFIX:=/usr/local/kong}
 
   if [[ "$2" == "docker-start" ]]; then
-    chown kong "$PREFIX"
-    su kong -c "kong prepare -p '$PREFIX'"
-    
-    setcap cap_net_raw=+ep /usr/local/openresty/nginx/sbin/nginx
+    kong prepare -p "$PREFIX"
+    chown -R kong "$PREFIX"
 
-    chmod 777 /proc/self/fd/1
-    chmod 777 /proc/self/fd/2
+    # workaround for https://github.com/moby/moby/issues/31243
+    chmod o+w /proc/self/fd/1
+    chmod o+w /proc/self/fd/2
 
-    su kong -c "/usr/local/openresty/nginx/sbin/nginx \
-      -p '$PREFIX' \
-      -c nginx.conf"
+    if [ ! -z ${SET_CAP_NET_RAW} ] \
+        || has_transparent "$KONG_STREAM_LISTEN" \
+        || has_transparent "$KONG_PROXY_LISTEN" \
+        || has_transparent "$KONG_ADMIN_LISTEN";
+    then
+      setcap cap_net_raw=+ep /usr/local/openresty/nginx/sbin/nginx
+    fi
+
+    exec su-exec kong /usr/local/openresty/nginx/sbin/nginx \
+      -p "$PREFIX" \
+      -c nginx.conf
   fi
 fi
 
