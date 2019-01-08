4f432b60dccd96e8d86413b4b7482e93350b1bad
Fazle Rabby <fazlerabby7@gmail.com>
2019-01-09 00:54:43 +0600

    Kong 1.0.0 image based on debian:stretch

---
+++ debian/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+export KONG_NGINX_DAEMON=off
+
+has_transparent() {
+  echo "$1" | grep -E "[^\s,]+\s+transparent\b" >/dev/null
+}
+
+if [[ "$1" == "kong" ]]; then
+  PREFIX=${KONG_PREFIX:=/usr/local/kong}
+
+  if [[ "$2" == "docker-start" ]]; then
+    kong prepare -p "$PREFIX"
+    chown -R kong "$PREFIX"
+
+    # workaround for https://github.com/moby/moby/issues/31243
+    chmod o+w /proc/self/fd/1
+    chmod o+w /proc/self/fd/2
+
+    if [ ! -z ${SET_CAP_NET_RAW} ] \
+        || has_transparent "$KONG_STREAM_LISTEN" \
+        || has_transparent "$KONG_PROXY_LISTEN" \
+        || has_transparent "$KONG_ADMIN_LISTEN";
+    then
+      setcap cap_net_raw=+ep /usr/local/openresty/nginx/sbin/nginx
+    fi
+
+    exec su-exec kong /usr/local/openresty/nginx/sbin/nginx \
+      -p "$PREFIX" \
+      -c nginx.conf
+  fi
+fi
+
+exec "$@"
