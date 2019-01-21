0bdb65f36642bd57e3d31c5eab1ecadc66aa112b
Zhang Xiang(RDDEV) <xzhang.rd@best-inc.com>
2019-01-21 15:16:13 +0800

    Update docker-entrypoint.sh in centos

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -18,12 +18,31 @@ if [[ "$1" == "kong" ]]; then
     chmod o+w /proc/self/fd/1
     chmod o+w /proc/self/fd/2
 
+    set_cap_net_raw=false
+    set_cap_net_bind_service=false
+
     if [ ! -z ${SET_CAP_NET_RAW} ] \
         || has_transparent "$KONG_STREAM_LISTEN" \
         || has_transparent "$KONG_PROXY_LISTEN" \
         || has_transparent "$KONG_ADMIN_LISTEN";
     then
+      set_cap_net_raw=true
+    fi
+    
+    if [ ! -z ${SET_CAP_NET_BIND_SERVICE} ];
+    then
+      set_cap_net_bind_service=true
+    fi
+
+    if $set_cap_net_raw&&$set_cap_net_bind_service;
+    then
       setcap cap_net_raw,cap_net_bind_service=+ep /usr/local/openresty/nginx/sbin/nginx
+    elif $set_cap_net_raw;
+    then
+      setcap cap_net_raw=+ep /usr/local/openresty/nginx/sbin/nginx
+    elif $set_cap_net_bind_service;
+    then
+      setcap cap_net_bind_service=+ep /usr/local/openresty/nginx/sbin/nginx
     fi
 
     exec su-exec kong /usr/local/openresty/nginx/sbin/nginx \
