946d839d9b315e7e9e3031432e0efc1b422cd668
Colin Hutchinson <hutchic@users.noreply.github.com>
2019-02-01 11:07:52 -0500

    feat(tests) moving the tests to a bash script (#222)
    * feat(tests) moved tests into a bash script with accompanying Makefile
    
    * fix(tests) alpine and centos compatible test
    
    * Added comments indentifying tests
    
    Since we want to add more tests, we should properly document and separate the tests

---
+++ tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set +e
+
+####################################################
+# Test the proper version was buid
+pushd $BASE
+version_given="$(grep 'ENV KONG_VERSION' Dockerfile | awk '{print $3}' | tr -d '[:space:]')"
+version_built="$(docker run -ti --rm kong-$BASE kong version | tr -d '[:space:]')"
+
+if [[ "$version_given" != "$version_built" ]]; then
+  echo "Kong version mismatch:";
+  echo "\tVersion given is $version_given";
+  echo "\tVersion built is $version_built";
+  exit 1;
+fi
+
+####################################################
+# Test LuaRocks is functional for installing rocks
+docker run -ti kong-$BASE luarocks install version
+
+popd
+
+####################################################
+# Validate Kong is running as the Kong user
+pushd compose
+docker-compose up -d
+until docker-compose ps | grep compose_kong_1 | grep -q "Up"; do sleep 1; done
+
+docker-compose exec kong ps aux | sed -n 2p | grep -q kong
+if [ $? -ne 0 ]; then
+  echo "Kong is not running as the Kong user";
+  echo "\tRunning instead as ";
+  docker-compose exec kong ps aux | sed -n 2p
+  exit 1;
+fi
