508d9b6277e1ede68549750e0ce29402ca212696
Thijs Schreijer <thijs@thijsschreijer.nl>
2019-02-01 09:57:57 +0100

    Added comments indentifying tests
    Since we want to add more tests, we should properly document and separate the tests

--- tests.sh
+++ tests.sh
@@ -2,6 +2,8 @@
 
 set +e
 
+####################################################
+# Test the proper version was buid
 pushd $BASE
 version_given="$(grep 'ENV KONG_VERSION' Dockerfile | awk '{print $3}' | tr -d '[:space:]')"
 version_built="$(docker run -ti --rm kong-$BASE kong version | tr -d '[:space:]')"
@@ -13,10 +15,14 @@ if [[ "$version_given" != "$version_built" ]]; then
   exit 1;
 fi
 
+####################################################
+# Test LuaRocks is functional for installing rocks
 docker run -ti kong-$BASE luarocks install version
 
 popd
 
+####################################################
+# Validate Kong is running as the Kong user
 pushd compose
 docker-compose up -d
 until docker-compose ps | grep compose_kong_1 | grep -q "Up"; do sleep 1; done
