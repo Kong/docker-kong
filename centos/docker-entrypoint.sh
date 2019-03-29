f91ba4cde0b35688cd766c96b306c5b2ce0f4521
Colin Hutchinson <chutchic@gmail.com>
2019-03-29 20:37:23 +0000

    fix(openshift) move the workaround to only apply to transparent proxying

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -14,10 +14,6 @@ if [[ "$1" == "kong" ]]; then
     shift 2
     kong prepare -p "$PREFIX" "$@"
 
-    # workaround for https://github.com/moby/moby/issues/31243
-    chmod o+w /proc/self/fd/1
-    chmod o+w /proc/self/fd/2
-
     if [ "$(id -u)" != "0" ]; then
       exec /usr/local/openresty/nginx/sbin/nginx \
         -p "$PREFIX" \
@@ -28,6 +24,9 @@ if [[ "$1" == "kong" ]]; then
           || has_transparent "$KONG_PROXY_LISTEN" \
           || has_transparent "$KONG_ADMIN_LISTEN";
       then
+        # workaround for https://github.com/moby/moby/issues/31243
+        chmod o+w /proc/self/fd/1
+        chmod o+w /proc/self/fd/2
         setcap cap_net_raw=+ep /usr/local/openresty/nginx/sbin/nginx
       fi
       chown -R kong:0 /usr/local/kong
