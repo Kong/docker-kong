d9e62eaa3f69e0a18d0269e721d71c1d8e728332
Colin Hutchinson <chutchic@gmail.com>
2019-07-11 18:20:36 +0000

    fix(swarm) fix docker swarm so we can upgrade in place

--- tests.sh
+++ tests.sh
@@ -23,21 +23,22 @@ popd
 pushd swarm
 docker swarm init
 KONG_DOCKER_TAG=kong:1.0 docker stack deploy -c docker-compose.yml kong
-until curl -I localhost:8001 | grep 'Server: openresty';  do
+until docker ps | grep kong:1.0 | grep -q healthy;  do
   docker stack ps kong
-  sleep 5
+  docker service ps kong_kong
+  sleep 10
 done
 
 docker stack deploy -c docker-compose.yml kong
 sleep 5
-until docker stack ps kong | grep kong_kong | grep kong:latest | grep -q Ready; do
+until docker ps | grep kong:1.0 | grep -q healthy; do
   docker stack ps kong
   docker service ps kong_kong
-  sleep 5
+  sleep 10
 done
+
 sleep 5
 curl -I localhost:8001 | grep 'Server: openresty'
-exit 123
 
 docker stack rm kong
 sleep 10
