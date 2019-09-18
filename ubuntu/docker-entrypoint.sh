db72d6ca107bdde09d3edf5ed395691775f187ef
Colin Hutchinson <hutchic@users.noreply.github.com>
2019-09-18 12:23:54 -0400

    feat(arm64) build Kong ubuntu images including arm64 (#297)

---
+++ ubuntu/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export KONG_NGINX_DAEMON=off
+
+if [[ "$1" == "kong" ]]; then
+  PREFIX=${KONG_PREFIX:=/usr/local/kong}
+
+  if [[ "$2" == "docker-start" ]]; then
+    kong prepare -p "$PREFIX"
+
+    exec /usr/local/openresty/nginx/sbin/nginx \
+      -p "$PREFIX" \
+      -c nginx.conf
+  fi
+fi
+
+exec "$@"
