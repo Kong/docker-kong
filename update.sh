e1b50ef21512da94c1ac94e0dcac60352d558839
Colin Hutchinson <chutchic@gmail.com>
2020-02-11 20:08:32 +0000

    chore(ci) adjust the Dockerfile's so they can be scripted to update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 if ! [ "$1" ]
 then
@@ -29,49 +29,30 @@ function die() {
 
 hub --version &> /dev/null || die "hub is not in PATH. Get it from https://github.com/github/hub"
 
-git stash
-git checkout "$prev_tag"
-if [ "$prev_tag" = "master" ]
-then
-   git pull
-fi
-git checkout -B release/$version
+#git stash
+#git checkout "$prev_tag"
+#if [ "$prev_tag" = "master" ]
+#then
+#   git pull
+#fi
+#git checkout -B release/$version
 
-sed "s,ENV KONG_VERSION .*,ENV KONG_VERSION $version," centos/Dockerfile > centos/Dockerfile.new
-mv centos/Dockerfile.new centos/Dockerfile
+sed -i -e "s/VERSION='$version'/VERSION='$prev_tag'/" */Dockerfile
 
-sed "s,ENV KONG_VERSION .*,ENV KONG_VERSION $version," rhel/Dockerfile > rhel/Dockerfile.new
-mv rhel/Dockerfile.new rhel/Dockerfile
+pushd alpine
+   URL=$(grep "URL='http" Dockerfile | grep -v enterprise | awk -F\' '{ print $2 $3 $4 }' | sed 's/$VERSION/'$prev_tag'/g')
+   curl -f -L -o asset.new $URL
+   new_sha=$(sha256sum asset.new | cut -b1-64)
+   
+   URL=$(grep "URL='http" Dockerfile | grep -v enterprise | awk -F\' '{ print $2 $3 $4 }' | sed 's/$VERSION/'$version'/g')
+   curl -f -L -o asset.old $URL
+   old_sha=$(sha256sum asset.old | cut -b1-64)
+   
+   sed -i -e 's/'$old_sha'/'$new_sha'/g' Dockerfile
+   rm asset.old asset.new
+popd
 
-sed "s,ENV KONG_VERSION .*,ENV KONG_VERSION $version," alpine/Dockerfile > alpine/Dockerfile.new
-mv alpine/Dockerfile.new alpine/Dockerfile
-
-if [ -e ubuntu/Dockerfile ]
-then
-   sed "s,ENV KONG_VERSION .*,ENV KONG_VERSION $version," ubuntu/Dockerfile > ubuntu/Dockerfile.new
-   mv ubuntu/Dockerfile.new ubuntu/Dockerfile
-fi
-
-apk="kong-$version.amd64.apk.tar.gz"
-
-if ! curl -f -L -o "$apk" "https://bintray.com/kong/kong-alpine-tar/download_file?file_path=$apk"
-then
-   apk="kong-$version.apk.tar.gz"
-   curl -f -L -o "$apk" "https://bintray.com/kong/kong-alpine-tar/download_file?file_path=$apk" || {
-      rm -f "$apk"
-      echo "****************************************"
-      echo "Failed to download Alpine package."
-      echo "Are the release artifact successfully deployed in Bintray?"
-      echo "If so, did their URL change? (update the Dockerfiles then!)"
-      echo "****************************************"
-      exit 1
-   }
-fi
-
-alpinesha=$(sha256sum "$apk" | cut -b1-64)
-
-sed "s,ENV KONG_SHA256 .*,ENV KONG_SHA256 $alpinesha," alpine/Dockerfile > alpine/Dockerfile.new
-mv alpine/Dockerfile.new alpine/Dockerfile
+exit 123
 
 echo "****************************************"
 git diff
