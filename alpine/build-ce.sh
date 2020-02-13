01d203f755f26036e80221770e2273a59b0af1b1
Colin Hutchinson <chutchic@gmail.com>
2020-02-13 15:08:34 +0000

    chore(refactor) take a stab at moving the logic out of the Dockerfile into build scripts

---
+++ alpine/build-ce.sh
@@ -0,0 +1,2 @@
+KONG_SHA="5af1178111958b2e325c5b18690f4e7ddf064d28139ff38188b1e2e432ea99ff  kong.tar.gz"
+URL="https://bintray.com/kong/kong-alpine-tar/download_file?file_path=kong-2.0.0.amd64.apk.tar.gz"
