61cd260ba36af547029ebc11d5fac3ac2fc6c5bf
Colin Hutchinson <chutchic@gmail.com>
2020-04-06 21:04:56 +0000

    chore(*) bump to Kong 2.0.3

--- ubuntu/build-ce.sh
+++ ubuntu/build-ce.sh
@@ -1,2 +1,2 @@
-VERSION=${VERSION:-2.0.1} && \
+VERSION=${VERSION:-2.0.3} && \
 curl -L "https://bintray.com/kong/kong-deb/download_file?file_path=kong-$VERSION.xenial.$(dpkg --print-architecture).deb" -o /tmp/kong.deb
