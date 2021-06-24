c5af0d260b03da56a51f4400a7b292256044a2b7
Thijs Schreijer <thijs@thijsschreijer.nl>
2021-06-24 20:21:06 +0200

    chore(*) whitespace and consistency

--- rhel/docker-entrypoint.sh
+++ rhel/docker-entrypoint.sh
@@ -4,26 +4,26 @@ set -Eeo pipefail
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+# "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
-	local def="${2:-}"
-	# Do not continue if _FILE env is not set
-	if ! [ "${!fileVar:-}" ]; then
-		return
-	elif [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
-	fi
-	local val="$def"
-	if [ "${!var:-}" ]; then
-		val="${!var}"
-	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
-	fi
-	export "$var"="$val"
-	unset "$fileVar"
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  # Do not continue if _FILE env is not set
+  if ! [ "${!fileVar:-}" ]; then
+    return
+  elif [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
 }
 
 export KONG_NGINX_DAEMON=${KONG_NGINX_DAEMON:=off}
--- submit.sh
+++ submit.sh
@@ -2,8 +2,8 @@
 set -e
 
 gawk --version &> /dev/null || {
-   echo "gawk is required to run this script."
-   exit 1
+  echo "gawk is required to run this script."
+  exit 1
 }
 
 mode=
@@ -11,60 +11,60 @@ version=
 force=
 
 function usage() {
-   echo "usage: $0 <-p|-m> <version>"
-   echo "   -p for patch release (x.y.Z)"
-   echo "   -m for minor release (x.Y.0)"
-   echo "   -r for release candidate (x.Y.0rcZ)"
-   echo "example: $0 -p 1.1.2"
+  echo "usage: $0 <-p|-m> <version>"
+  echo "   -p for patch release (x.y.Z)"
+  echo "   -m for minor release (x.Y.0)"
+  echo "   -r for release candidate (x.Y.0rcZ)"
+  echo "example: $0 -p 1.1.2"
 }
 
 while [ "$1" ]
 do
-   case "$1" in
-   --help)
-      usage
-      exit 0
-      ;;
-   -p)
-      mode=patch
-      ;;
-   -r)
-      mode=rc
-      ;;
-   -m)
-      mode=minor
-      ;;
-   -f)
-      force=yes
-      ;;
-   [0-9]*)
-      version=$1
-      ;;
-   esac
-   shift
+  case "$1" in
+  --help)
+    usage
+    exit 0
+    ;;
+  -p)
+    mode=patch
+    ;;
+  -r)
+    mode=rc
+    ;;
+  -m)
+    mode=minor
+    ;;
+  -f)
+    force=yes
+    ;;
+  [0-9]*)
+    version=$1
+    ;;
+  esac
+  shift
 done
 
 function red() {
-   echo -e "\033[1;31m$@\033[0m"
+  echo -e "\033[1;31m$@\033[0m"
 }
 
 function die() {
-   red "*** $@"
-   echo "See also: $0 --help"
-   echo
-   exit 1
+  red "*** $@"
+  echo "See also: $0 --help"
+  echo
+  exit 1
 }
 
 hub --version &> /dev/null || die "hub is not in PATH. Get it from https://github.com/github/hub"
 
 if [ "$mode" = "" ]
 then
-   die "Error: mode flag is mandatory"
+  die "Error: mode flag is mandatory"
 fi
 
 if ! [ "$version" ]
 then
-   die "Error: missing version"
+  die "Error: missing version"
 fi
 
 git checkout master
@@ -72,26 +72,26 @@ git pull
 
 if ! grep -q "$version" alpine/Dockerfile
 then
-   if [[ "$force" = "yes" ]]
-   then
-      echo "Forcing to use the tag even though it is not in master."
+  if [[ "$force" = "yes" ]]
+  then
+    echo "Forcing to use the tag even though it is not in master."
 
-      git checkout "$version"
+    git checkout "$version"
 
-      if ! grep -q "$version$" alpine/Dockerfile
-      then
-         die "Error: version in build script doesn't match required version."
-      fi
-   else
-      echo "****************************************"
-      echo "Error: this script should be run only after the"
-      echo "desired release is merged in master of docker-kong."
-      echo ""
-      echo "For making releases based on old versions,"
-      echo "Use -f to override and submit from the tag anyway."
-      echo "****************************************"
-      die "Failed."
-   fi
+    if ! grep -q "$version$" alpine/Dockerfile
+    then
+      die "Error: version in build script doesn't match required version."
+    fi
+  else
+    echo "****************************************"
+    echo "Error: this script should be run only after the"
+    echo "desired release is merged in master of docker-kong."
+    echo ""
+    echo "For making releases based on old versions,"
+    echo "Use -f to override and submit from the tag anyway."
+    echo "****************************************"
+    die "Failed."
+  fi
 fi
 
 xy=${version%.*}
@@ -99,20 +99,20 @@ z=${version#$xy.}
 
 if [ "$mode" = "rc" ]
 then
-   rc=${version#*rc}
-   z=${z%rc*}
+  rc=${version#*rc}
+  z=${z%rc*}
 fi
 
 commit=$(git show "$version" | grep "^commit" | head -n 1 | cut -b8-48)
 
 if [ "$mode" = "patch" ]
 then
-   prev="$xy.$[z-1]"
-   prevcommit=$(git show "$prev" | grep "^commit" | head -n 1 | cut -b8-48)
+  prev="$xy.$[z-1]"
+  prevcommit=$(git show "$prev" | grep "^commit" | head -n 1 | cut -b8-48)
 elif [ "$mode" = "rc" -a "$rc" -gt 1 ]
 then
-   prev="$xy.${z}rc$[rc-1]"
-   prevcommit=$(git show "$prev" | grep "^commit" | head -n 1 | cut -b8-48)
+  prev="$xy.${z}rc$[rc-1]"
+  prevcommit=$(git show "$prev" | grep "^commit" | head -n 1 | cut -b8-48)
 fi
 
 rm -rf submit
@@ -129,128 +129,128 @@ git checkout -b release/$version
 
 if [ "$mode" = "patch" ]
 then
-   sed "s|$prev-alpine|$version-alpine|;
-        s|$prev-centos|$version-centos|;
-        s|$prev-ubuntu|$version-ubuntu|;
-        s|$prev,|$version,|;
-        s|$prevcommit|$commit|;
-        s|refs/tags/$prev|refs/tags/$version|" library/kong > library/kong.new
-   mv library/kong.new library/kong
+  sed "s|$prev-alpine|$version-alpine|;
+       s|$prev-centos|$version-centos|;
+       s|$prev-ubuntu|$version-ubuntu|;
+       s|$prev,|$version,|;
+       s|$prevcommit|$commit|;
+       s|refs/tags/$prev|refs/tags/$version|" library/kong > library/kong.new
+  mv library/kong.new library/kong
 
 elif [ "$mode" = "rc" -a "$rc" -gt 1 ]
 then
-   sed "s|$prev-alpine|$version-alpine|;
-        s|$prev-centos|$version-centos|;
-        s|$prev-ubuntu|$version-ubuntu|;
-        s|, ${xy}rc$[rc-1]|, ${xy}rc${rc}|;
-        s|$prev,|$version,|;
-        s|$prevcommit|$commit|;
-        s|refs/tags/$prev|refs/tags/$version|" library/kong > library/kong.new
-   mv library/kong.new library/kong
+  sed "s|$prev-alpine|$version-alpine|;
+       s|$prev-centos|$version-centos|;
+       s|$prev-ubuntu|$version-ubuntu|;
+       s|, ${xy}rc$[rc-1]|, ${xy}rc${rc}|;
+       s|$prev,|$version,|;
+       s|$prevcommit|$commit|;
+       s|refs/tags/$prev|refs/tags/$version|" library/kong > library/kong.new
+  mv library/kong.new library/kong
 
 elif [ "$mode" = "rc" -a "$rc" -eq 1 ]
 then
-   gawk '
-      BEGIN {
-         reset = 0
-         not_yet_first = 1
+  gawk '
+    BEGIN {
+      reset = 0
+      not_yet_first = 1
+    }
+    /^Tags/ {
+      if (not_yet_first == 1) {
+        not_yet_first = 0
+        before_first = 1
       }
-      /^Tags/ {
-         if (not_yet_first == 1) {
-            not_yet_first = 0
-            before_first = 1
-         }
+    }
+    {
+      if (before_first == 1) {
+        v = "'$version'"
+        xy = "'$xy'"
+        commit = "'$commit'"
+        print "Tags: " v "-alpine, " v
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Directory: alpine"
+        print "Architectures: amd64"
+        print ""
+        print "Tags: " v "-ubuntu"
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Directory: ubuntu"
+        print "Architectures: amd64, arm64v8"
+        print ""
+        print "Tags: " v "-centos"
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Constraints: !aufs"
+        print "Directory: centos"
+        print "Architectures: amd64"
+        print ""
+        print
+        before_first = 0
+      } else {
+        print
       }
-      {
-         if (before_first == 1) {
-            v = "'$version'"
-            xy = "'$xy'"
-            commit = "'$commit'"
-            print "Tags: " v "-alpine, " v
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Directory: alpine"
-            print "Architectures: amd64"
-            print ""
-            print "Tags: " v "-ubuntu"
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Directory: ubuntu"
-            print "Architectures: amd64, arm64v8"
-            print ""
-            print "Tags: " v "-centos"
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Constraints: !aufs"
-            print "Directory: centos"
-            print "Architectures: amd64"
-            print ""
-            print
-            before_first = 0
-         } else {
-            print
-         }
-      }
-   ' library/kong > library/kong.new
-   mv library/kong.new library/kong
+    }
+  ' library/kong > library/kong.new
+  mv library/kong.new library/kong
 
 elif [ "$mode" = "minor" ]
 then
-   gawk '
-      BEGIN {
-         reset = 0
-         not_yet_first = 1
+  gawk '
+    BEGIN {
+      reset = 0
+      not_yet_first = 1
+    }
+    /^Tags/ {
+      if (not_yet_first == 1) {
+        not_yet_first = 0
+        before_first = 1
       }
-      /^Tags/ {
-         if (not_yet_first == 1) {
-            not_yet_first = 0
-            before_first = 1
-         }
+    }
+    /Tags: .*[0-9]rc[0-9].*/ {
+      in_rc_tag = 1
+    }
+    /^ *$/ {
+      if (in_rc_tag == 1) {
+        reset = 1
       }
-      /Tags: .*[0-9]rc[0-9].*/ {
-         in_rc_tag = 1
+    }
+    {
+      if (before_first == 1) {
+        v = "'$version'"
+        xy = "'$xy'"
+        commit = "'$commit'"
+        print "Tags: " v "-alpine, " v ", " xy ", latest"
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Directory: alpine"
+        print "Architectures: amd64"
+        print ""
+        print "Tags: " v "-ubuntu, " xy "-ubuntu"
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Directory: ubuntu"
+        print "Architectures: amd64, arm64v8"
+        print ""
+        print "Tags: " v "-centos, " xy "-centos"
+        print "GitCommit: " commit
+        print "GitFetch: refs/tags/" v
+        print "Constraints: !aufs"
+        print "Directory: centos"
+        print "Architectures: amd64"
+        print ""
+        before_first = 0
+      } else if (!(in_rc_tag == 1)) {
+        gsub(", latest", "")
+        print
       }
-      /^ *$/ {
-         if (in_rc_tag == 1) {
-            reset = 1
-         }
+      if (reset == 1) {
+        in_rc_tag = 0
+        reset = 0
       }
-      {
-         if (before_first == 1) {
-            v = "'$version'"
-            xy = "'$xy'"
-            commit = "'$commit'"
-            print "Tags: " v "-alpine, " v ", " xy ", latest"
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Directory: alpine"
-            print "Architectures: amd64"
-            print ""
-            print "Tags: " v "-ubuntu, " xy "-ubuntu"
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Directory: ubuntu"
-            print "Architectures: amd64, arm64v8"
-            print ""
-            print "Tags: " v "-centos, " xy "-centos"
-            print "GitCommit: " commit
-            print "GitFetch: refs/tags/" v
-            print "Constraints: !aufs"
-            print "Directory: centos"
-            print "Architectures: amd64"
-            print ""
-            before_first = 0
-         } else if (!(in_rc_tag == 1)) {
-            gsub(", latest", "")
-            print
-         }
-         if (reset == 1) {
-            in_rc_tag = 0
-            reset = 0
-         }
-      }
-   ' library/kong > library/kong.new
-   mv library/kong.new library/kong
+    }
+  ' library/kong > library/kong.new
+  mv library/kong.new library/kong
 fi
 
 echo "****************************************"
@@ -262,7 +262,7 @@ echo "(Answering y will commit, push the branch, and submit the PR)"
 read
 if ! [ "$REPLY" == "y" ]
 then
-   exit 1
+  exit 1
 fi
 
 git commit -av -m "kong $version"
