4e7c6d9b0c843ac5b68f876c0cfe2fbc1de379b4
hutchic <chutchic@gmail.com>
2021-08-31 15:50:20 +0000

    chore(security): add updates to our Dockerfile's

--- tests/01-image.test.sh
+++ tests/01-image.test.sh
@@ -23,7 +23,6 @@ function run_test {
   fi
   popd
 
-  set +x
   if [[ ! -z "${SNYK_SCAN_TOKEN}" ]]; then
     docker scan --login --token "${SNYK_SCAN_TOKEN}"
     docker scan --accept-license --exclude-base --file $BASE/Dockerfile kong-$BASE
